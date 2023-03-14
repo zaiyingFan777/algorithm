66e621df69b884df03a9aad4fccd0fe553cc87d3
zaiying fan <zaiying.fan@realai.ai>
2023-03-14 11:07:41 +0800

    feat: 02-数学之美-maxSubArr4种方法

--- 2023算法通关之路/02-数学之美.js
+++ 2023算法通关之路/02-数学之美.js
@@ -274,3 +274,253 @@ var threeSumClosest2 = function(nums, target) {
   return res;
 };
 
+// 53-maxSubArray 最大子序列和
+// 解法一 暴力法（超时）
+// 一般情况下，可以先用暴力法进行分析，再一步步进行优化。
+// 思路
+// 首先来试一下最直接的方法，就是计算所有的子序列和，然后取出最大值。定义Sum[i,…,j]为数组A中第i个元素到第j个元素的和，其中0≤i≤j < n，遍历所有可能的Sum[i,…,j]即可。
+// 复杂度分析
+// 时间复杂度：O(n^2)，其中n为数组长度。
+// 空间复杂度：O(1)
+// 代码
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  var n = nums.length;
+  var maxSum = -Infinity;
+  // 空间复杂度0(1)
+  var total = 0;
+  for(var i = 0; i < n; i++) {
+    total = 0;
+    for(var j = i; j < n; j++) {
+      // 这里j从i开始，是因为每个数本身也要成为连续子序列和这个大集合的元素之一
+      total += nums[j];
+      maxSum = Math.max(maxSum, total);
+    }
+  }
+  return maxSum;
+};
+
+// 解法二　分治法
+// 解法一的空间复杂度非常理想，但是时间复杂度有点高，该怎么优化呢？
+// 思路
+// 这道题实际上是一个可以用多种方法解决的题目，如果你想到了上面的解法，我想面试官肯定不会满意，而如果你一时想不到更好的解决方法的话，有两种方法可以帮助你理清思路。
+// 1.举几个简单的例子。这种方法通常适用于很复杂的问题，人们一时间难以发现其中的规律。树和链表等题目使用这种方法比较好，搭配画图来将问题进行可视化的展现效果会更好。
+// 2.将大问题拆解为若干子问题，通过解决子问题，以及探寻子问题和大问题之间的关系来解决。
+// 这里我们采用第2种方法。
+// 假如先把数组平均分成左、右两部分，那么此时有3种情况。
+//   ·最大子序列全部在数组左部分，不妨用left表示。
+//   ·最大子序列全部在数组右部分，不妨用right表示。
+//   ·最大子序列横跨数组左右部分，不妨用crossMaxSum表示。
+// 对于前两种情况，相当于将原问题转化成了规模更小的同类问题。对于第3种情况，由于已知循环的起点（即中点），只需要向左、向右分别找出左边和右边的最大子序列，
+// 那么当前最大子序列就是向左能够达到的最大子序列+nums[mid]+向右能够达到的最大子序列。因此，一个思路就是每次都将数组分成左、右两部分，
+// 然后分别计算上面这3种情况的最大子序列和，最后返回最大值即可。
+// 见图片 02-images/02-maxSubArray.jpg
+// 复杂度分析
+// 时间复杂度：从上图可以看出每层的节点在[n,2n]之间，且一共有 logn层，因此时间复杂度为O(nlogn)，其中n为数组长度。
+// 空间复杂度：空间复杂度取决于函数调用栈的深度，故空间复杂度为O(logn)，其中n为数组长度，这也可以从上图直观地感受到。
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  // 左右子数组的最大子序列和
+  var maxSubArrayHelper = function(nums, lo, hi) {
+    // 函数base出口：lo > hi，返回负无穷（因为没有要去对比的数，只能返回负无穷），lo === hi返回自身即可
+    if (lo > hi) return -Infinity;
+    // 找到中间值
+    var mid = (lo + hi) >> 1; // Math.floor((lo + hi) / 2)
+    // 分治：左侧、右侧数组最大子序列和
+    var left = maxSubArrayHelper(nums, lo, mid - 1);
+    var right = maxSubArrayHelper(nums, mid + 1, hi);
+    // 找出nums[mid] + 从mid到左侧的最大子序列和 + 从mid到右侧的最大子序列和
+    // 定义左侧数组、右侧数组的累加值
+    // var leftSuffixMaxSum = 0, rightPrefixMaxSum = 0;
+    // 为何要初始化为0，因为如果mid左侧的是负数都没必要再想加进去，只会越来越小
+    var leftSuffixMaxSum = 0, rightPrefixMaxSum = leftSuffixMaxSum;
+    // 处理累加的变量
+    var total = 0;
+    // 计算从lo到mid-1的最大子序列和，左侧需要反着求和
+    for(var i = mid - 1; i >= lo; i--) {
+      total += nums[i];
+      leftSuffixMaxSum = Math.max(total, leftSuffixMaxSum);
+    }
+    total = 0;
+    // 计算右侧从mid+1到hi
+    for(var i = mid + 1; i <= hi; i++) {
+      total += nums[i];
+      rightPrefixMaxSum = Math.max(total, rightPrefixMaxSum);
+    }
+    return Math.max(left, right, leftSuffixMaxSum + nums[mid] + rightPrefixMaxSum);
+  }
+  return maxSubArrayHelper(nums, 0, nums.length - 1);
+}
+
+// 解法三 动态规划
+// 思路
+// 我们重新思考一下这个问题，观察能否将其拆解为规模更小的问题，并找出递推关系来解决？
+// 不妨假设问题Q(list,i)表示[list中以索引i结尾的最大子序列和]，那么原问题就转化为Q(list,i)中的最大值，其中i =0,1,2,…,n-1。
+// 继续来看一下Q(list,i)和Q(list,i-1)的关系，即如何根据Q(list,i-1)推导出Q(list,i)。
+// 1.如果Q(list,i-1)>0，则表示以索引i-1结尾的最大子序列和大于0，因此nums[i]一定要和Q(list,i-1)部分结合，这样才能使结果更优，即Q(list,i)=Q(list, i-1)＋nums[i]，这是一种贪心的思想。
+// 2.如果Q(list,i-1)≤0，则为了使结果更优，nums[i]应该不与Q(list,i-1)相加。ps: 因为无论正数还是负数还是0与一个负数相加只会越来越小，所以不如从nums[i]从新算
+// 分析到这里，递推关系就很明朗了，即Q(list,i)=max(0,Q(list,i-1))+nums[i]。
+// 见图片 ./02-images/02-maxSubArray-02.jpg
+// 复杂度分析
+// 时间复杂度：O(n)，其中n为数组长度。
+// 空间复杂度：O(1)。
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  var n = nums.length;
+  // maxSum最终结果，curMaxSum为每次计算的当前的最大值
+  var maxSum = nums[0], curMaxSum = maxSum;
+  for(var i = 1; i < n; i++) {
+    // 以nums[i]为结尾的「最大子数组和」为dp[i]
+    // 如果Q(list, i)以索引i结尾的最大子序列和，如果Q(list, i - 1) > 0则需要将Nums[i]与Q(list, i - 1)相结合
+    // 如果Q(list, i - 1) <= 0，则为了结果更优，Nums[i]应该不与Q(list, i - 1)相加。
+    curMaxSum = Math.max(curMaxSum + nums[i], nums[i]);
+    // nums[0..i]中的「最大的子数组和」为dp[i]
+    maxSum = Math.max(maxSum, curMaxSum);
+  }
+  return maxSum;
+}
+
+// 解法四 前缀和
+// 思路
+// 下面从数学分析的角度来看一下这个题目。定义函数S(i)，它的功能是计算从0（包括0）开始加到i（包括i）的值，即S(i)=list[0]+list[1]+…+list[i]。
+// 那么S(j)-S(i-1)就等于从i开始（包括i）加到j（包括j）的值，这在数学上被称为前缀和求差，因此实际上只需要遍历一次计算出所有的S(i)，其中i等于0,1,2,…,n-1，
+// 然后利用前缀和就可以计算出任意区间的和。这种算法的时间复杂度为O(n)，空间复杂度为O(1)。
+// 分析：我们找到最小的前缀和，然后maxSum = Math.max(sum - minSum, maxSum)，这样就可以找到最大的子数列和
+// 比如      -1 -2 -3 4
+// sum      -1 -3 -6 -2
+// maxSum   -1 -1 -1 4
+// minSum   -1 -3 -6 -6
+// 复杂度分析
+// 时间复杂度：O(n)，其中n为数组长度。
+// 空间复杂度：O(1)。
+// 代码
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  var n = nums.length;
+  // 这里为何要用minSum为0，我们计算的时候从i为0开始，我们需要有初始值，只能让minSum为0，因为sum和minSum为前缀和我们需要从0开始
+  // maxSum为nums[0]，这样我们第一次比较的时候需要有默认值
+  var sum = 0, minSum = sum, maxSum = nums[0];
+  for(var i = 0; i < n; i++) {
+    sum += nums[i];
+    // 用前缀和 - 最小前缀和 得到最大子序列和
+    maxSum = Math.max(maxSum, sum - minSum);
+    // 最小前缀和
+    minSum = Math.min(minSum, sum);
+  }
+  return maxSum;
+}
+
+// labuladong版本
+// https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247497299&idx=1&sn=ea5adedf25ac46a84018a6def2e284d5&scene=21#wechat_redirect
+// 动态规划思路：
+// 按照我们常规的动态规划思路，一般是这样定义dp数组：nums[0..i]中的「最大的子数组和」为dp[i]。
+// 实际上是不行的，因为子数组一定是连续的，按照我们当前dp数组定义，并不能保证nums[0..i]中的最大子数组与nums[i+1]是相邻的，也就没办法从dp[i]推导出dp[i+1]。
+// 对于这类子数组问题，我们就要重新定义dp数组的含义：以nums[i]为结尾的「最大子数组和」为dp[i]。
+// 这种定义之下，想得到整个nums数组的「最大子数组和」，不能直接返回dp[n-1]，而需要遍历整个dp数组：
+// int res = Integer.MIN_VALUE;
+// for (int i = 0; i < n; i++) {
+//   res = Math.max(res, dp[i]);
+// }
+// return res;
+// 依然使用数学归纳法来找状态转移关系：假设我们已经算出了dp[i-1]，如何推导出dp[i]呢？
+// 可以做到，dp[i]有两种「选择」，要么与前面的相邻子数组连接，形成一个和更大的子数组；要么不与前面的子数组连接，自成一派，自己作为一个子数组。
+// 如何选择？既然要求「最大子数组和」，当然选择结果更大的那个啦：
+// 要么自成一派，要么和前面的子数组合并
+// dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+// 综上，我们已经写出了状态转移方程，就可以直接写出解法了：
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  var n = nums.length;
+  if (n === 0) return 0;
+  // 定义：dp[i]记录以nums[i]为结尾的「最大子数组和」
+  // 空间复杂度为O(N)
+  var dp = new Array(n).fill(0);
+  // base state
+  // 第一个元素前面没有子数组
+  dp[0] = nums[0];
+  // 状态转移方程
+  for(var i = 1; i < n; i++) {
+    // 要么自称一派，要么和前面的子数组合并
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+  }
+  var res = -Infinity;
+  for(var i = 0; i < n; i++) {
+    res = Math.max(res, dp[i]);
+  }
+  return res;
+}
+// 自我改动
+// var maxSubArray = function(nums) {
+//   var n = nums.length;
+//   if (n === 0) return 0;
+//   // 定义：dp[i]记录以nums[i]为结尾的「最大子数组和」
+//   var dp = new Array(n).fill(0);
+//   // base state
+//   // 第一个元素前面没有子数组
+//   dp[0] = nums[0];
+//   // nums[0..i]中的「最大的子数组和」为res。
+//   var res = nums[0];
+//   // 状态转移方程
+//   for(var i = 1; i < n; i++) {
+//     // 要么自称一派，要么和前面的子数组合并
+//     dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+//     res = Math.max(dp[i], res);
+//   }
+//   return res;
+// }
+// 上面空间复杂度
+// 以上解法时间复杂度是 O(N)，空间复杂度也是 O(N)，较暴力解法已经很优秀了，不过注意到dp[i]仅仅和dp[i-1]的状态有关
+var maxSubArray = function(nums) {
+  var n = nums.length;
+  if (n === 0) return 0;
+  // base case
+  var dp_0 = nums[0];
+  var dp_1 = 0, res = dp_0;
+  for(var i = 1; i < n; i++) {
+    // 以nums[i]为结尾的「最大子数组和」为dp[i]
+    dp_1 = Math.max(nums[i], dp_0 + nums[i]);
+    dp_0 = dp_1;
+    // 顺便计算最大的结果 也就是nums[0..i]中的最大子数组和
+    res = Math.max(res, dp_1);
+  }
+  return res;
+}
+
+// labuladong前缀和
+// 以nums[i]为结尾的最大子序列的和：preSum[i+1](sum[0..i])减去最小的preSum[i](最小的sum[0..i-1])
+// 以nums[i]为结尾的最大子数组之和是多少？其实就是preSum[i+1] - min(preSum[0..i])。
+var maxSubArray = function(nums) {
+  var n = nums.length;
+  // 我们创建前缀和,需要建立n+1的长度，这样preSum[i+1]就是sum[0..i]
+  var preSum = new Array(n + 1).fill(0);
+  // 循环创建前缀和
+  for(var i = 1; i <= n; i++) {
+    preSum[i] = preSum[i - 1] + nums[i - 1];
+  }
+  // 声明res和minVal(最小的前缀和)
+  var res = -Infinity;
+  var minVal = Infinity;
+  // 循环找出以nums[i]为结尾的最大子序列的和
+  for(var i = 0; i < n; i++) {
+    // 找到最小的前缀和
+    minVal = Math.min(minVal, preSum[i]);
+    // 计算sum[0..i](preSum[0..i+1])与minSum的差
+    res = Math.max(res, preSum[i + 1] - minVal);
+  }
+  return res;
+}
